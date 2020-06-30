dad6b6bc109595ffafdb428554d3725cfd7bef62
GitPAV <pierre.vincs@gmail.com>
2020-06-30 19:07:31 +0200

    added curvy top

--- src/app/components/home-page/home-page.vue
+++ src/app/components/home-page/home-page.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <p>Bienvenue ma gueule</p>
+        <div class="chevronContainer">
+            <div id="chevronUpLeft"></div>
+            <div id="chevronUpMiddle"></div>             
+            <div id="chevronUpRight"></div> 
+        </div>
         <div class="curvyBackground"></div> 
         <div class="chevronContainer">
-            <div id="chevronLeft"></div>
-            <div id="chevronMiddle"></div>             
-            <div id="chevronRight"></div> 
+            <div id="chevronDownLeft"></div>
+            <div id="chevronDownMiddle"></div>             
+            <div id="chevronDownRight"></div> 
         </div>
     </div>
 </template>
