98248104d2a9d1bb6c99a232b487ead2335b0cb2
Julia Hu <juliahukoshi@gmail.com>
2020-07-14 15:53:14 +0200

    wip home band

---
+++ src/app/components/home-page/components/home-band-component/home-band-component.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home-background">
+        <img src="@/assets/logotj.png" alt="" class="logos">
+        <img src="@/assets/logo515copy.png" alt="" class="logos">
+    </div>
+</template>
+<script>
+    export default {
+        name: 'HomeBand',
+    }
+</script>
+<style lang="scss">
+  .home-background {
+    background: url('../../../../../assets/home-band-bg.jpg') no-repeat center center;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    filter: brightness(80%);
+    width: 100%;
+    height: 450px;
+}
+</style>
--- src/app/components/home-page/home-page.vue
+++ src/app/components/home-page/home-page.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        <p>Bienvenue ma gueule</p>
+        <HomeBand />
     </div>
 </template>
+<script>
+  import HomeBand from '@/app/components/home-page/components/home-band-component/home-band-component.vue'
+
+  export default {
+    name: 'Home',
+    components: {
+      HomeBand
+    }
+  }
+</script>
