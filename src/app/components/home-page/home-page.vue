96d79346fd608e6e3b241e0b2317e6577ba6d5dd
Julia Hu <juliahukoshi@gmail.com>
2020-07-14 19:51:10 +0200

    wip event pzge

--- src/app/components/home-page/home-page.vue
+++ src/app/components/home-page/home-page.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <HomeBand />
+        <EventComponent />
     </div>
 </template>
 <script>
   import HomeBand from '@/app/components/home-page/components/home-band-component/home-band-component.vue'
+  import EventComponent from '@/app/shared/components/event-component/event-component.vue'
 
   export default {
     name: 'Home',
     components: {
-      HomeBand
+      HomeBand,
+      EventComponent
     }
   }
 </script>
--- src/app/shared/components/event-component/event-component.vue
+++ src/app/shared/components/event-component/event-component.vue
@@ -1,1 +1,43 @@
-<!-- TESTER ICI DE RECEVOIR LA DATA DEPUIS FIREBASE-->
+<template>
+    <div class="event">
+        <img :src="image" alt="" id="band-image"/>
+        <div id="event-infos">
+            <p>{{ type }}</p>
+            <p>{{ artist }}</p>
+            <p>{{ presentation }}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+        name: 'EventComponent',
+        data() {
+            return {
+                type: 'Concert',
+                date: '20 septembre 2020',
+                artist: 'Christy & Flopy',
+                origin: 'New Orléans',
+                year: '2009',
+                presentation: 'Texte de présentation',
+                time: '21h00',
+                price: 15,
+                image: require('@/assets/photo-band-test.jpg')
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    .event {
+        color: #393939;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        #band-image {
+            width: 30%;
+        }
+        #event-infos {
+            width: 40%;
+        }
+    }
+</style>
