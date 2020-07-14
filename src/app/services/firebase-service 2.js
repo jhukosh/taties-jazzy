3465da28beb112e34cfe8501ebb1418f7a9fae89
Julia Hu <juliahukoshi@gmail.com>
2020-07-14 17:04:01 +0200

    wip

---
+++ src/app/services/firebase-service 2.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase'
+const db = firebase.firestore()
+
+export const firebaseService = {
+    methods: {
+        getEvents: () => new Promise((resolve, reject) => {
+            const result = []
+            db.collection("events").get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                result.push(doc.data())
+                });
+                resolve(result)
+                reject('No event found in databse')
+            })
+        }),
+        saveNewEvent: (newEvent) => new Promise((resolve, reject) => {
+            db.collection("events").add(newEvent)
+            .then(function() {
+                console.log("Document successfully written!");
+            })
+            .catch(function(error) {
+                console.error("Error writing document: ", error);
+            });
+        })
+    }
+}
