e3d266423460130540eebbecd39b9f58b4ef38f3
Julia Hu <juliahukoshi@gmail.com>
2020-07-25 17:02:14 +0200

    WIP footer

--- src/app/components/main-layout/components/footer-component/footer-component.vue
+++ src/app/components/main-layout/components/footer-component/footer-component.vue
@@ -1,12 +1,12 @@
 <template>
     <div id="footer">
         <div class="section">
-            <div>
+            <div class="first-section-logos">
                 <img src="@/assets/logotj.png" alt="" class="logos">
                 <img src="@/assets/logo515copy.png" alt="" class="logos">
             </div>
-            <div>
-                <ul>
+            <div class="first-section-links">
+                <ul class="links-list">
                     <li>
                         <router-link to="/association">Association</router-link>
                     </li>
@@ -33,27 +33,5 @@
     }
 </script>
 <style lang="scss">
-    #footer {
-        align-items: center;
-        background-color: #0e0e0e;
-        border-top: 11px solid #fd2232;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-        height: 200px;
-        width: 100%;
-
-        .section {
-            display: flex;
-            flex-direction: row;
-            width: 30%;
-            .logos {
-                display: flex;
-                flex-direction: column;
-                height: 63px;
-                object-fit: contain;
-                width: 50%;
-            }
-        }
-    }
+   @import "footer-component.scss";
 </style>
