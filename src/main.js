ce62bff67754416fcb994f19672c7193d4aee912
Julia Hu <juliahukoshi@gmail.com>
2020-07-26 13:25:59 +0200

    removed config

--- src/main.js
+++ src/main.js
@@ -2,18 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
+import { config } from '../firebaseConfig'
 
 Vue.config.productionTip = false
 
 const config = {
-  apiKey: "AIzaSyB7fPlUiZx5LwYvSJea3mg456yl2llZzqY",
-  authDomain: "taties-jazzy.firebaseapp.com",
-  databaseURL: "https://taties-jazzy.firebaseio.com",
-  projectId: "taties-jazzy",
-  storageBucket: "taties-jazzy.appspot.com",
-  messagingSenderId: "506211563747",
-  appId: "1:506211563747:web:5e1cdd6ad2f31038b52dd6",
-  measurementId: "G-LRHXV2FZQE"
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId
 }
 
 firebase.initializeApp(config)
