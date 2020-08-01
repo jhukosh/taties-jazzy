94d129dd668ab3fa60c2f8e1a3e4a5a923751698
Julia Hu <juliahukoshi@gmail.com>
2020-08-01 17:13:07 +0200

    Revert "bootstraped contact form"
    This reverts commit d07d589ec484fa1fa16de0e5be93c8dbe8d1886e.

--- src/app/components/contact-page/contact-page.vue
+++ src/app/components/contact-page/contact-page.vue
@@ -1,16 +1,5 @@
 <template>
     <div class="main">
-        <p>Contact page bitch</p>
-        <ContactFormComponent />
-        <MapComponent />
+        <p>Contact page</p>
     </div>
 </template>
-
-<script>
-    import ContactFormComponent from '@/app/components/contact-page/components/contact-form-component/contact-form-component.vue'
-    import MapComponent from '@/app/components/contact-page/components/map-component/map-component.vue'
-    export default {
-        name: 'ContactPage',
-        components: { ContactFormComponent, MapComponent }
-    }
-</script>
