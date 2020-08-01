925ac81f4504af63d88c6986b8545617f797d7a0
Julia Hu <juliahukoshi@gmail.com>
2020-08-01 17:23:21 +0200

    form ok

---
+++ src/app/components/contact-page/components/contact-form-component/contact-form-component.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent>
+    <h1>Get Started</h1>
+    <div>
+      <label for="name">Nom</label>
+      <input v-model="sendMessageForm.name" type="text" placeholder="Nom" id="name" />
+    </div>
+    <div>
+      <label for="firstname">Prénom</label>
+      <input v-model="sendMessageForm.firstname" type="text" placeholder="Prénom" id="firstname" />
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input v-model="sendMessageForm.email" type="text" placeholder="you@email.com" id="email" />
+    </div>
+    <div>
+      <label for="message">Message</label>
+      <input v-model="sendMessageForm.message" type="password" placeholder="Votre message" id="message" />
+    </div>
+    <button @click="sendMessageForm()" class="button">Envoyer</button>
+  </form>
+</template>
+<script>
+  import { sendMessage } from '@/core/services/send-message-service.js'
+  export default {
+    name: 'ContactFormComponent',
+    data() {
+      return { 
+        sendMessageForm: {
+          name: '',
+          firstname: '',
+          email: '',
+          message: ''
+        }
+      }
+    },
+    methods: {
+      sendMessageForm() {
+        sendMessage()
+        console.log('form', this.sendMessageForm)
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
+
