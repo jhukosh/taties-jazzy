922f9371c8e98c09582598a8ba06bd36ae1bce20
Pierre <pierre.vincs@gmail.com>
2020-08-05 21:33:13 +0200

    new curvy background done, for home page and association pages

--- src/app/components/association-page/association-page.vue
+++ src/app/components/association-page/association-page.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="main">
         <p>Association page</p>
-        <p>Association pagge</p>
-        <img src="~/assets/customSVG/curvy-bg-top.svg" alt="">
-        <img src="~/assets/customSVG/curvy-bg-bot.svg" alt="">
+
+        <img src="../../../assets/customSVG/curvy-bg-test.svg" alt="">
+        <img src="../../../assets/customSVG/curvy-bg-testHome.svg" alt="">
     </div>
 </template>
