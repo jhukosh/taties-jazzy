4aa8442ce363e1d47e8c9428d36d4ab1e77b045c
Pierre <pierre.vincs@gmail.com>
2020-08-11 18:58:30 +0200

    association page WIP

--- src/app/components/association-page/components/about-us-component/about-us-component.vue
+++ src/app/components/association-page/components/about-us-component/about-us-component.vue
@@ -1,5 +1,7 @@
 <template>
     <div id="About-us-main">
+
+        <!-- Header -->
         <div id="About-us-header">
             <div class="blackHorizontalSeparator"></div>
             <div id="Who-we-are">
@@ -10,6 +12,46 @@
                 </p>
             </div>
             <div class="blackHorizontalSeparator"></div>
+        </div>
+
+        <!-- Content -->
+        <div id="About-us-presentation">
+            
+            <div class="about-us-content">
+                <div class="about-us-content-image">
+                    <!-- <img src="../../../../../assets/trumpet.png" alt="background trompet decoration"> -->
+                    <img  class="article-image"  src="../../../../../assets/association-image-1.png" alt="association founder picture">
+                </div>
+                <div class="about-us-article">
+                    <div class="article-header">
+                        Les Taties Jazzy, c’est avant tout une ambiance, une atmosphère. La rencontre des artistes aux sonorités qui swing, des rythmes qui balances au long des partitions improvisées...
+                    </div>
+                    <div class="article-handwritting">
+                        ‘’fous de jazz ‘’
+                    </div>
+                    <div class="article-content">
+                        Le projet des Taties Jazzy est de proposer un endroit, et des conditions appropriés aux artistes passionnés de Jazz. Le but est qu’ils puissent se produirent et offrir une performance de qualité, dans un cadre idéal pour les amateurs de Jazz. <br><br>
+                        Les aficionados du genre pourrons venir apprécier et découvrir des talents, mais aussi des inspirations revisités, dans un cadre dédiés. <br><br>
+                        Vous l’aurez compris, avec les Taties Jazzy, le swing est à l’honneur !
+                    </div>
+                </div>
+            </div>
+
+            <div class="about-us-content">
+                <div class="about-us-article" id="Article-second-part">
+                    <div class="article-next-header">
+                        ‘’Des potes ‘’<br>
+                    </div>
+                    <div class="article-content">
+                        <p>Nous sommes un groupe d’amis dont l’envie était, de créer un endroit où les artistes de jazz peuvent venirs à la rencontre d’un public passionné et de partager un moment joyeux et festif. <br><br></p>
+                        <p>Nous sommes nous mêmes de grands amateurs de musiques (dont le Jazz, principalement), et certains d’entre nous s’adonnent même à cette passion, seul ou dans un groupe. La musique prend donc une grande place dans nos vies et est un élément qui nous lie.<br><br></p>
+                        <p>C’est donc à l’image de notre amitié et de cette passion que nous avons voulu créer les Taties Jazzy: des copines, un brins délurées, à la joie de vivre débordantes et passionnées de musique qui swing et qui balance! </p>
+                    </div>
+                </div>
+                <div class="about-us-content-image">
+                    <img  class="article-image-last"  src="../../../../../assets/association-image-2.png" alt="found at festival picture">
+                </div>
+            </div>
         </div>
     </div>
 </template>
