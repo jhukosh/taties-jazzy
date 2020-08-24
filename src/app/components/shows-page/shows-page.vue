46a768ceef6b1b8443c0694ee23f593fbf135e44
Pierre <pierre.vincs@gmail.com>
2020-08-24 19:37:41 +0200

    show v for done, item style done, wip concert-page

--- src/app/components/shows-page/shows-page.vue
+++ src/app/components/shows-page/shows-page.vue
@@ -1,14 +1,11 @@
 <template>
     <div id="Show-page-component">
         <TitleBackgroundComponent :background_image="path_to_image" :background_title="background_title_value" />
-        <div id="DEV">
-            <p>Shows page</p>
-            <p>{{ shows }}</p>
-            <button @click="getEvent">Click me (show-page)</button>
-        </div>
 
         <div id="Show-page-container">
-            <EventComponent :parentData="shows"/>
+            <div class="show-item" v-for="show in shows" :key="show.id">
+                <EventComponent :parentData="show"/>
+            </div>
         </div>
         
     </div>
@@ -38,12 +35,9 @@ export default {
         }
     },
     methods: {
-        getEvent() {
-            console.log('result', this.shows)
-        },
         async getEventsTest() {
             this.getEvents().then( res => {
-                this.shows = res.filter( show => show.type === "show")[0];
+                this.shows = res.filter( show => show.type === "show");
                 console.log('SHOWS',this.shows)
             })
         }
