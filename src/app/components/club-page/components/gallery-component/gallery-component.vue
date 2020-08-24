c38278929dc3a158b7ccad1982e99bd08b68dda5
mallemouche christelle <christellema08@gmail.com>
2020-08-24 14:58:06 +0200

    wip scss portfolio

--- src/app/components/club-page/components/gallery-component/gallery-component.vue
+++ src/app/components/club-page/components/gallery-component/gallery-component.vue
@@ -1,20 +1,27 @@
 <template>
-    <div class="gallery-component-main">
-        <div id="container">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+    <div id="groscontainer">
+        <div id="container1">
+            <div class="bebe" v-for="(item, index) in 4" :key="item">
+                <img :src="require(`@/assets/club-page-assets/gallery-component/img-left${index+1}.png`)"/>
+            </div>
         </div>
-       
+
+        <div id="container2">
+            <div class="nana" v-for="(item, index) in 7" :key="item">
+                <img :src="require(`@/assets/club-page-assets/gallery-component/img-right${index+1}.png`)"/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    export default {
+        data: function () {
+            return {
+            }
+        }
+    }
+
 </script>
 
 <style>
