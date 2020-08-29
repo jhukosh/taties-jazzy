67dc2d8456865cec92db921cc93fa45ac91eb80f
Julia Hu <julia@artips.fr>
2020-08-29 21:47:44 +0200

    offers component ok

--- src/app/components/home-page/components/offers-component/offers-component.vue
+++ src/app/components/home-page/components/offers-component/offers-component.vue
@@ -1,12 +1,24 @@
 <template>
-<div id="offers-component">
-  <div>
-    <img :src="picture" alt="" />
-  </div>
-  <div>
-    <h4>{{ title }}</h4>
-    <p><strong>{{ firstParagraphStart }}</strong>{{ firstParagraph }}</p>
-    <p>{{ secondParagraph }} <span>{{ secondParagraphEnd }}</span></p>
+<div id="Offers-component">
+  <img src="@/assets/reversed-trumpet.png" alt="" class="offers-trumpet-icon"/>
+  <div class="offers-content">
+    <div class="offers-content-img">
+      <img :src="picture" alt="" />
+    </div>
+    <div class="offers-content-infos">
+      <h4>{{ title }}</h4>
+      <p>
+        <strong>{{ firstParagraphStart }}</strong>
+        {{ firstParagraph1 }}
+        <strong>{{ clubName }}</strong>
+        {{ firstParagraph2 }}
+        <strong>{{ firstParagraph3 }}</strong>
+      </p>
+      <p class="offers-content-infos-second-paragraph">{{ secondParagraph }} <span>{{ secondParagraphEnd }}</span></p>
+      <div>
+        <button class="black-button  black-button-txt">VOIR PLUS</button>
+      </div>
+    </div>
   </div>
 </div>
 </template>
@@ -18,9 +30,12 @@ export default {
       picture: require('@/assets/offers.jpg'),
       title: "Des concerts en folie",
       firstParagraphStart: "Les Taties Jazzy",
-      firstParagraph: "vous accueillent chez Smain au 'R' club, dans un cadre chaleureux et atypique où vous pourrez voir se produire des artistes talentueux",
+      firstParagraph1: "vous accueillent chez Smain au",
+      clubName: "'R' club",
+      firstParagraph2: ", dans un cadre chaleureux et atypique où vous pourrez voir se produire des",
+      firstParagraph3: "artistes talentueux",
       secondParagraph: "Tous les vendredis et samedis soirs, de 19h jusqu'au bout de la nuit",
-      secondParagraphEnd: "(Enfin... OOh30, c'est déjà pas mal !)"
+      secondParagraphEnd: "(Enfin... 00h30, c'est déjà pas mal !)"
     }
   }
 }
