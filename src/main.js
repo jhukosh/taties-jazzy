6dfba19602d3d3a299a7cd4b279d0813c176b8e0
Julia Hu <julia@artips.fr>
2020-08-30 23:17:00 +0200

    test deploy 4

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import firebase from 'firebase/app'
+//import firebase from 'firebase/app'
 import 'setimmediate';
-import { config } from '../firebaseConfig'
+//import { config } from '../firebaseConfig'
 
 Vue.config.productionTip = false
 
-firebase.initializeApp(config)
+//firebase.initializeApp(config)
 
 new Vue({
   router,
