b11d5c8ab23caa2ad5c4fc77c4e4f9ddc13c98dd
Julia Hu <julia@artips.fr>
2020-08-31 11:53:11 +0200

    add deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '/les-taties-jazzy/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/taties-jazzy/'
+    : '/',
   css: {
     loaderOptions: {
       sass: {
