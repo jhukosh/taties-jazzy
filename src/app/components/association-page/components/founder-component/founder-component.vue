b89c4c384332ea570c8a83a48bdedcc791de75a2
Slevin nuttin <kevin.nuttin@gmail.com>
2020-09-05 11:57:55 +0200

    wip responsive association page

--- src/app/components/association-page/components/founder-component/founder-component.vue
+++ src/app/components/association-page/components/founder-component/founder-component.vue
@@ -1,81 +1,75 @@
 <template>
-    <div id="Founder-component">
-
-        <!-- Founder picture-->
-        <div id="Founder-picture">
-            <img src="@/assets/association-page-assets/association-staff.png" alt="">
-        </div>
-
-        <!-- Background graphical text -->
-        <div class="background-big-text-top">
-            <p>Taties</p>
-        </div>
-
-        <!-- Founder content -->
-        <div id="Founder-content">
-            <div class="founder">
-                <div class="founder-header align-right">
-                        Lêah VolzaTuri
-                    <div class="sub-header">
-                        Présidente Fondatrice
-                    </div>
-                </div>
-                <br><br>
-                <div class="founder-content align-right">
-                    “Salut les copains! Je m’appelle Leah et je suis Directrice artistique et Designer d’expérience . Dans la vie, j’ai un tas de passion!
-                    L’astronomie -dont je m’informe régulièrement des avancées dans les revus et vidéos dédiées-, les animaux -j’ai, un temps, envisagée de travailler avec eux et est effectuée un stage en clinique vétérinaire.  C’était dans une autre vie!-, l’art - la photo et le cinéma surtout-, les sorties entre copains… 
-                    Mais ce qui m’aide à me lever le matin; ce qui rythme mes journées et me donne du baume au coeur; ce qui m’anime et me pousse en avant, c’est la musique! Les rythmes jazzy entraînant et la soul dans ses voix de baronnes!
-                    <br><br>
-                    <div class="founder-one-subtitle">
-                        L’idée des Taties Jazzies? 
-                    </div>
-                </div>
-                <div class="founder-content align-right">
-                    Et l’idée de fonder les Taties Jazzies avec Loreleï et les copains se trouve être opportunité énorme pour moi de pouvoir vivre une expérience dont la musique serait le moteur principal. 
-                    Y’a du boulot, mais on est motivés! 
-                </div>
-            </div>
-
-            <div class="vertical-grey-separator founders-separator"></div>
-
-            <div class="founder">
-                <div class="founder-header align-left">
-                        Loreleï Servain
-                    <div class="sub-header">
-                        Membre fondatrice
-                    </div>
-                </div>
-                <br><br>
-                <div class="founder-content align-left">
-                    “Salut la famille! Moi c’est Loreleï. Mon métier, c’est Ing.&amp; son. 
-                    Pour moi, la musique fait intégralement partie de ma vie. J’ai choisis mon job dans le secteur du spectacle afin de pouvoir vivre pleinement ma passion. a côté, je pratique régulièrement le violon et le chant. 
-                    J’aime également exprimer ma créativité dans le dessin, j’ai d’ailleur beaucoup aimé bosser sur notre logo. Je suis quelqu’un d’enjoué, et j’aime vivre les choses à fond. 
-                    <br><br>
-                    <div class="founder-one-subtitle">
-                        L’idée des Taties Jazzies? 
-                    </div>
-                </div>
-                <div class="founder-content align-left">
-                    Ca a été une évidence pour Leah et moi. Nous sommes amies depuis des années et partageons depuis longtemps l’amour de la musique et du Jazz. 
-                    Et puis, une idée en entraînant une autre… Et nous voilà entrain de réfléchir au concept de notre asso! C’est un vrai défis à relever et j’ai hâte de partager notre travail avec vous!” 
-                </div>
-            </div>
-        </div>
-
-        <!-- Background graphical text -->
-        <div class="background-big-text-bottom">
-            <p>Jazzy</p>
-        </div>
-
+  <div id="Founder-component">
+    <!-- Founder picture-->
+    <div id="Founder-picture">
+      <img src="@/assets/association-page-assets/association-staff.png" alt />
     </div>
+
+    <!-- Background graphical text -->
+    <div class="background-big-text-top">
+      <p>Taties</p>
+    </div>
+
+    <!-- Founder content -->
+    <div id="Founder-content">
+      <div class="founder">
+        <div class="founder-header align-right">
+          Lêah VolzaTuri
+          <div class="sub-header">Présidente Fondatrice</div>
+        </div>
+        <br />
+        <br />
+        <div class="founder-content align-right">
+          “Salut les copains! Je m’appelle Leah et je suis Directrice artistique et Designer d’expérience. Dans la vie, j’ai un tas de passion!
+          L’astronomie -dont je m’informe régulièrement des avancées dans les revus et vidéos dédiées-, les animaux -j’ai, un temps, envisagée de travailler avec eux et est effectuée un stage en clinique vétérinaire. C’était dans une autre vie!-, l’art - la photo et le cinéma surtout-, les sorties entre copains…
+          Mais ce qui m’aide à me lever le matin; ce qui rythme mes journées et me donne du baume au coeur; ce qui m’anime et me pousse en avant, c’est la musique! Les rythmes jazzy entraînants et la soul dans ses voix de baronnes!
+          <br />
+          <br />
+          <div class="founder-one-subtitle">L’idée des Taties Jazzies?</div>
+        </div>
+        <div class="founder-content align-right">
+          Et l’idée de fonder les Taties Jazzies avec Loreleï et les copains se trouve être opportunité énorme pour moi de pouvoir vivre une expérience dont la musique serait le moteur principal.
+          Y’a du boulot, mais on est motivés!
+        </div>
+      </div>
+
+      <div class="vertical-grey-separator founders-separator"></div>
+
+      <div class="founder">
+        <div class="founder-header align-left">
+          Loreleï Servain
+          <div class="sub-header">Membre fondatrice</div>
+        </div>
+        <br />
+        <br />
+        <div class="founder-content align-left">
+          “Salut la famille! Moi c’est Loreleï. Mon métier, c’est Ing.&amp; son.
+          Pour moi, la musique fait intégralement partie de ma vie. J’ai choisi mon job dans le secteur du spectacle afin de pouvoir vivre pleinement ma passion. A côté, je pratique régulièrement le violon et le chant.
+          J’aime également exprimer ma créativité dans le dessin, j’ai d’ailleurs beaucoup aimé bosser sur notre logo. Je suis quelqu’un d’enjoué, et j’aime vivre les choses à fond.
+          <br />
+          <br />
+          <div class="founder-one-subtitle">L’idée des Taties Jazzies?</div>
+        </div>
+        <div class="founder-content align-left">
+          Ca a été une évidence pour Leah et moi. Nous sommes amies depuis des années et partageons depuis longtemps l’amour de la musique et du Jazz.
+          Et puis, une idée en entraînant une autre… Et nous voilà en train de réfléchir au concept de notre asso! C’est un vrai défi à relever et j’ai hâte de partager notre travail avec vous!”
+        </div>
+      </div>
+    </div>
+
+    <!-- Background graphical text -->
+    <div class="background-big-text-bottom">
+      <p>Jazzy</p>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'FounderComponent',
-    }
+export default {
+  name: "FounderComponent",
+};
 </script>
 
 <style lang="scss">
-    @import "founder-component.scss";
+@import "founder-component.scss";
 </style>
